0a9d378fd5b8c96a6a6376e327b8597355c42bfd
Jason <598486501@qq.com>
2024-06-07 15:17:32 +0800

    update project structure and add nacos

--- nep-admin/src/main/java/com/neu/edu/controller/AdminController.java
+++ nep-admin/src/main/java/com/neu/edu/controller/AdminController.java
@@ -2,10 +2,8 @@ package com.neu.edu.controller;
 
 import com.neu.edu.common.annotation.LogOperation;
 import com.neu.edu.common.page.PageData;
-import com.neu.edu.common.utils.ExcelUtils;
 import com.neu.edu.common.utils.Result;
 import com.neu.edu.dto.AdminDTO;
-import com.neu.edu.excel.AdminsExcel;
 import com.neu.edu.common.constant.Constant;
 import com.neu.edu.common.validator.AssertUtils;
 import com.neu.edu.common.validator.ValidatorUtils;
@@ -22,20 +20,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 import java.util.Map;
 
 
 /**
- *
- *
  * @author FEI Bo neufeibo@gmail.com
  * @since 1.0.0 2024-06-06
  */
 @RestController
 @RequestMapping("demo/admin")
-@Api(tags="")
+@Api(tags = "")
 public class AdminController {
     @Autowired
     private AdminsService adminsService;
@@ -43,13 +37,13 @@ public class AdminController {
     @GetMapping("page")
     @ApiOperation("分页")
     @ApiImplicitParams({
-        @ApiImplicitParam(name = Constant.PAGE, value = "当前页码，从1开始", paramType = "query", required = true, dataType="int") ,
-        @ApiImplicitParam(name = Constant.LIMIT, value = "每页显示记录数", paramType = "query",required = true, dataType="int") ,
-        @ApiImplicitParam(name = Constant.ORDER_FIELD, value = "排序字段", paramType = "query", dataType="String") ,
-        @ApiImplicitParam(name = Constant.ORDER, value = "排序方式，可选值(asc、desc)", paramType = "query", dataType="String")
+            @ApiImplicitParam(name = Constant.PAGE, value = "当前页码，从1开始", paramType = "query", required = true, dataType = "int"),
+            @ApiImplicitParam(name = Constant.LIMIT, value = "每页显示记录数", paramType = "query", required = true, dataType = "int"),
+            @ApiImplicitParam(name = Constant.ORDER_FIELD, value = "排序字段", paramType = "query", dataType = "String"),
+            @ApiImplicitParam(name = Constant.ORDER, value = "排序方式，可选值(asc、desc)", paramType = "query", dataType = "String")
     })
     @RequiresPermissions("demo:admin:page")
-    public Result<PageData<AdminDTO>> page(@ApiIgnore @RequestParam Map<String, Object> params){
+    public Result<PageData<AdminDTO>> page(@ApiIgnore @RequestParam Map<String, Object> params) {
         PageData<AdminDTO> page = adminsService.page(params);
 
         return new Result<PageData<AdminDTO>>().ok(page);
@@ -58,7 +52,7 @@ public class AdminController {
     @GetMapping("{id}")
     @ApiOperation("信息")
     @RequiresPermissions("demo:admin:info")
-    public Result<AdminDTO> get(@PathVariable("id") Long id){
+    public Result<AdminDTO> get(@PathVariable("id") Long id) {
         AdminDTO data = adminsService.get(id);
 
         return new Result<AdminDTO>().ok(data);
@@ -68,7 +62,7 @@ public class AdminController {
     @ApiOperation("保存")
     @LogOperation("保存")
     @RequiresPermissions("demo:admin:save")
-    public Result save(@RequestBody AdminDTO dto){
+    public Result save(@RequestBody AdminDTO dto) {
         //效验数据
         ValidatorUtils.validateEntity(dto, AddGroup.class, DefaultGroup.class);
 
@@ -81,7 +75,7 @@ public class AdminController {
     @ApiOperation("修改")
     @LogOperation("修改")
     @RequiresPermissions("demo:admin:update")
-    public Result update(@RequestBody AdminDTO dto){
+    public Result update(@RequestBody AdminDTO dto) {
         //效验数据
         ValidatorUtils.validateEntity(dto, UpdateGroup.class, DefaultGroup.class);
 
@@ -94,7 +88,7 @@ public class AdminController {
     @ApiOperation("删除")
     @LogOperation("删除")
     @RequiresPermissions("demo:admin:delete")
-    public Result delete(@RequestBody Long[] ids){
+    public Result delete(@RequestBody Long[] ids) {
         //效验数据
         AssertUtils.isArrayEmpty(ids, "id");
 
@@ -102,15 +96,4 @@ public class AdminController {
 
         return new Result();
     }
-
-    @GetMapping("export")
-    @ApiOperation("导出")
-    @LogOperation("导出")
-    @RequiresPermissions("demo:admin:export")
-    public void export(@ApiIgnore @RequestParam Map<String, Object> params, HttpServletResponse response) throws Exception {
-        List<AdminDTO> list = adminsService.list(params);
-
-        ExcelUtils.exportExcelToTarget(response, null, "", list, AdminsExcel.class);
-    }
-
 }

bf24fb00c89e2d5d2baad148a7e4b72245a78575
Bo FEI <neufeibo@gmail.com>
2024-06-25 18:16:05 +0800

    更改请求头名字

--- nep-common/src/main/java/com/neu/edu/common/interceptor/UserInfoInterceptor.java
+++ nep-common/src/main/java/com/neu/edu/common/interceptor/UserInfoInterceptor.java
@@ -16,7 +16,7 @@ public class UserInfoInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 1. 获取登录用户信息
-        String userInfo = request.getHeader("user-info");
+        String userInfo = request.getHeader("user_info");
         // 2. 判断是否获取了用户
         if (StrUtil.isNotBlank(userInfo)) {
             Long userId = JwtUtils.parseToken(userInfo).getClaim("userId").asLong();
